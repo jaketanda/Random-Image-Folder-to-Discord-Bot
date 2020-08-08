8e8267e8cf48bce2bd465e248485a7254c337969
Jake Tanda <38367976+jaketanda@users.noreply.github.com>
2020-08-07 23:01:16 -0400

    Bug fix: issue where !random would crash the bot

--- index.js
+++ index.js
@@ -6,53 +6,45 @@ const config = require('./config.json');
 const fs = require('fs');
 const client = new Discord.Client();
 
-// Ready up the bot.
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-// When a user messages in the discord channel
 client.on('message', message => {
-    // return if a command is not being called
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-    // split command into command and arguments
+    
     const args = message.content.slice(config.prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+    const command = args.shift();
 
-    // instantiate variables
-    var number = -1; // number of items in the folder
-    var folder = -1; // the number of the folder in the folder array in config.json
+    var number = -1; 
+    var folder = -1; 
 
-    // loop to find the proper folder for the command
-    for (var i = 0; i < config.folder.length; i++) {
-        // if the command matches the current folder
-        if (command === config.folder[i] || command === "random") {
-            if (command === "random")
-            { i = Math.floor(Math.random() * config.folder.length); } // set it to a random folder.
+    if (config.folder.includes(command)) {
+        folder = config.folder.indexOf(command);
 
-            folder = i; // set the folder number to the current folder
-            
-            // get the number of files in the folder.
-            fs.readdir('./' + config.folder[i], (err, files) => {
-                number = files.length; // set the number of files to the number variable
+        fs.readdir('./' + config.folder[folder], (err, files) => {
+            number = files.length;
 
-                if (!args.length || command === "random")
-                { imageNumber = Math.floor(Math.random() * number) + 1; } // random integer of the images in the folder.
-                else // specific image in the folder.
-                { 
-                    imageNumber = args[0] * 1; 
-                    if (imageNumber > number)
-                    { return message.channel.send("Possible arguments: " + config.prefix + command + " [1-" + number + "]."); }
-                } 
+            if (!args.length) {
+                imageNumber = Math.floor(Math.random() * number) + 1;
+            } else {
+                imageNumber = args[0];
 
-                console.log(imageNumber);
-                message.channel.send ({files: ["./" + config.folder[folder] + "\\" + imageNumber + ".png"]} ) // attach the random png to the channel
-            });
-            return;
-        }
+                if (imageNumber > number) { 
+                    return message.channel.send("Possible arguments: " + config.prefix + command + " [1-" + number + "]."); 
+                }
+            }
+
+            message.channel.send ({files: ["./" + config.folder[folder] + "\\" + imageNumber + ".png"]} )
+        });
+    } else if (command === "random") {
+        folder = Math.floor(Math.random() * config.folder.length);
+        fs.readdir('./' + config.folder[folder], (err, files) => {
+            number = files.length;
+            imageNumber = Math.floor(Math.random() * number) + 1;
+            message.channel.send ({files: ["./" + config.folder[folder] + "\\" + imageNumber + ".png"]} )
+        });
     }
 });
 
-// login the bot
 client.login(config.token);
