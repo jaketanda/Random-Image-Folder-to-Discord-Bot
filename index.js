c39a3b1c848aa59cf7aae3f90c982c5a7f0f1a91
jaketanda <jakemtanda@gmail.com>
2021-10-14 18:19:07 -0700

    Update to current version of discord.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // random image folder to discord bot
 // jake tanda
 
-const Discord = require('discord.js');
+const { Client, Intents } =  require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
-const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
     
     const args = message.content.slice(config.prefix.length).split(/ +/);
